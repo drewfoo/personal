1af9110633b63cc1963ab93cee671122c13c0d62
Andrew Min <andrewkyungmin@gmail.com>
2018-10-24 09:52:04 -0700

    Working connections and setup to Mongo.db

--- routes/next.js
+++ routes/next.js
@@ -14,6 +14,7 @@ router.get('/next/:id', function(req, res){
     const message = cards[id][side];
     const templateData = { message };
 
+
     res.render('index', templateData)
 
     // if ( side === "") {
