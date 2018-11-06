22b7401b3e13b42f2db728b1c4f9cfc5358faf52
Andrew Min <andrewkyungmin@gmail.com>
2018-11-06 15:09:35 -0800

    Registration form and database access complete and working

--- models/user.js
+++ models/user.js
@@ -2,6 +2,7 @@
 //need to create a new schema with the schema object and pass the object of keys/values from the value and make sure we get all the data
 
 var mongoose = require('mongoose');
+var bcrypt = require('bcryptjs')
 var UserSchema = new mongoose.Schema({
     email: {
       type: String,
@@ -24,6 +25,22 @@ var UserSchema = new mongoose.Schema({
       required: true
     }
 });
+
+// function to run just prior to sending it to Mongo; has password
+// hook name 'save' mongoose keyword, before saving mongoose will run the anonymous function
+// this var user is the current instance of the user submission
+// 10 how many times to process the password
+UserSchema.pre('save', function(next) {
+  var user = this;
+  bcrypt.hash(user.password, 10, function(err, hash) {
+    if (err) {
+      return next(err);
+    }
+    user.password = hash;
+    next();
+  })
+});
+
 
 // need to export to use schema in repo, create new variable use model method and then it creates our schema.
 var User = mongoose.model('User', UserSchema);
