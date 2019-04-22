b0e65229f4c3e9fb71b8705ab2d0b7426a5585dc
andmin <andmin@andmin-m-c3g9.corp.appdynamics.com>
2019-04-22 15:54:34 -0700

    updated to include plays, changed playbook route

--- routes/playbook.js
+++ routes/playbook.js
@@ -10,90 +10,18 @@ const Section = require('../models/playbook');
 const Playbook = require('../models/playbook');
 const mid = require('../middleware');
 
-// // Main Playbook Route
-// router.get('/playbook', function(req,res,next){
-// 	Playbook.find().
-//     exec(function (error, playbook) {
-//       if (error) {
-//         return next(error);
-//       } else {
-//         let main = playbook.filter(function(book){
-//             return book.key === "overview";
-//         });
-//         let sub = playbook.filter(function(book){
-//             return book.key !== "overview";
-//         });
-//         // let mainSections = main[0];
-//         // let sectionContent = [];
-//         // let related = [];
-//         // for (i = 0 ; i < mainSections.detail.length ; i++){
-//         //     sectionContent.push(mainSections.detail[i]);
-//         // }
-//         // for (i = 0 ; i < sub.length ; i++){
-//         //     related.push({"name":sub[i].name, "heading":sub[i].heading})
-//         // }
-//         console.log(main);
-//         console.log(sub);
-//         // return res.render('playbook', { title: 'Playbook', 
-//         //     name: mainSections.name,
-//         //     heading: mainSections.heading,
-//         //     summary: mainSections.summary,
-//         //     sectionContent : sectionContent,
-//         //     relatedContent : related
-//     }})
-// });
-
-// // Playbook Section Routes
-// router.get('/playbook/:name', function(req,res,next){
-// 	Playbook.findOne( { name : req.params.name } )
-// 	.populate('plays').
-//     exec(function (error, playbook) {
-//       if (error) {
-//         return next(error);
-//       } else {
-// 					let sectionContent= [];
-// 					for ( i = 0 ; i < playbook.detail.length ; i++){
-// 						sectionContent.push(playbook.detail[i])
-//                     }
-//                     let relatedContent= [];
-//                     for ( i = 0 ; i < playbook.plays.length ; i++){
-//                         relatedContent.push({"name": playbook.plays[i].name, "heading": playbook.plays[i].heading})
-//                     }
-//                     console.log(relatedContent);
-//                 // console.log(playbook)	
-// 				// console.log(sectionContent);
-//         // let playbookList = [];
-//         // let playerList = [];
-//         // for (i = 0 ; i < playbook.detail.length ; i++ ){
-//         //   playbookList.push(playbook.detail[i]);}
-//         // for (i = 0 ; i < playbook.players.length ; i++ ){
-//         //   playerList.push(playbook.players[i]);   
-//         // }
-//         // console.log(playbook);
-//         // console.log(playbookList);
-//         // console.log(playerList);
-//         return res.render('playbook', { title: 'Playbook', 
-//             name: playbook.name,
-//             heading: playbook.heading,
-//             summary: playbook.summary,
-//             sectionContent: sectionContent,
-//             relatedContent: relatedContent
-//     });
-// }});
-// });
-
-
 // Playbook Route
-router.get('/playbook/:key', mid.requiresLogIn, function(req,res,next){
+router.get('/playbook/:key', mid.requiresLogIn, (req, res, next) => {
 	Playbook.findOne( { key : req.params.key } )
-	// .populate('plays').
-    .exec(function (error, playbook) {
+	.populate('plays').
+    exec ( (error, playbook) => {
       if (error) {
         return next(error);
       } else { 
         let sectionContent= [];
         let references= [];
         let faq= [];
+        let plays= [];
         for ( i = 0 ; i < playbook.section.length ; i++){
             sectionContent.push({
                 "name" : playbook.section[i].name,
@@ -104,17 +32,18 @@ router.get('/playbook/:key', mid.requiresLogIn, function(req,res,next){
             })
             references.push(playbook.section[i].references)
         }
-        // for ( i = 0 ; i < playbook.references.length ; i++){
-        //     referenceLinks.push(playbook.references[i])
-        // }
-        // flatten referencelinks array
-        console.log(sectionContent);
+        for ( i = 0 ; i < playbook.plays.length ; i++){
+          plays.push(playbook.plays[i].name
+          )
+        }
         let referenceLinks = [].concat.apply([], references);
+        console.log(plays);
         return res.render('playbook', {
             title: 'Playbook',
             sectionContent: sectionContent,
-            referenceLinks: referenceLinks
-          })
+            referenceLinks: referenceLinks,
+            playContent: plays
+        })
       }
     })
 })
